3d879eef62e6834b759b489b1a9cc990f8bb00ab
armemon <arahman.ar2820@gmail.com>
2022-01-08 11:42:20 +0500

    Uploading default files of React

---
+++ src/button.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Button from '@mui/material/Button';
+
+
+class Button1 extends React.Component {  //merge this class and React.Component
+
+    render() {
+        return (
+            <div>
+                <Button variant="contained">Hello World</Button>
+            </div>
+        )
+    }
+}
+
+export default Button1;
